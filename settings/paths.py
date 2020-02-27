9d7a27061eb12e788557478bc2ff899baae5900f
MarkAntipin <antipinsuperstar@yandex.ru>
2020-02-28 02:54:34 +0300

    add zip files response

--- settings/paths.py
+++ settings/paths.py
@@ -13,4 +13,4 @@ MODEL_DIR = Path(BASE_DIR, 'models')
 MODEL_PATH = Path(MODEL_DIR, 'coco.h5').as_posix()
 MODEL_DIR.parent.mkdir(parents=True, exist_ok=True)
 
-PRELOAD_IMAGE_PATH = Path(FILES_DIR, 'preload.jpg').as_posix()
+PRELOAD_IMAGE_PATH = Path(FILES_DIR, 'preload_hello.jpg').as_posix()

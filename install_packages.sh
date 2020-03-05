bc575f28eeaa71434749c304e8a4221a0c072ba7
MarkAntipin <antipinsuperstar@yandex.ru>
2020-03-05 15:13:45 +0300

    remove txt file and change on install_packages.sh

---
+++ install_packages.sh
@@ -0,0 +1,4 @@
+pip install -U torch==1.4 torchvision==0.5 -f https://download.pytorch.org/whl/torch_stable.html
+pip install cython pyyaml==5.1
+pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
+pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu100/index.html
